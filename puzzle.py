5a1c621e3b183608d7b716b86a5b0357a0f0653b
Camden Warncke <146666794+cjwarncke@users.noreply.github.com>
2024-08-08 09:47:49 -0400

    Create puzzle.py

---
+++ puzzle.py
@@ -0,0 +1,68 @@
+from queue import Queue
+
+
+def find_neighbors(puzzle, square):
+    row, column = square
+    neighbors = []
+    # Check left
+    if column > 0:
+        if puzzle[row][column-1] == '-':
+            neighbors.append((row, column-1))
+
+    # Check right
+    if column < len(puzzle[0]) - 1:
+        if puzzle[row][column+1] == '-':
+            neighbors.append((row, column+1))
+
+    # Check up
+    if row > 0:
+        if puzzle[row-1][column] == '-':
+            neighbors.append((row-1, column))
+
+    # Check down
+    if row < len(puzzle) - 1:
+        if puzzle[row+1][column] == '-':
+            neighbors.append((row+1, column))
+
+    return neighbors
+
+
+def solve_puzzle(board, source, destination):
+    q = Queue(maxsize=len(puzzle)*len(puzzle[0]))
+    visited = {source:None}
+    q.put(source)
+    while not q.empty() and destination not in visited:
+        curr_square = q.get()
+        neighbors = find_neighbors(puzzle, curr_square)
+        for neighbor in neighbors:
+            if neighbor not in visited:
+                visited[neighbor] = curr_square
+                q.put(neighbor)
+    print(curr_square)
+    print(q.get())
+
+
+    # Trace solution path
+    path = [destination]
+    curr_square = destination
+    while visited[curr_square] is not None:
+        parent = visited[curr_square]
+        path.append(parent)
+        curr_square = parent
+    path.reverse()
+    print(path)
+
+
+
+puzzle = [
+ ['-', '-', '-', '-', '-'],
+ ['-', '-', '#', '-', '-'],
+ ['-', '-', '-', '-', '-'],
+ ['#', '-', '#', '#', '-'],
+ ['-', '#', '-', '-', '-']
+]
+
+source = (0, 0)
+destination = (4, 0)
+
+solve_puzzle(puzzle, source, destination)
